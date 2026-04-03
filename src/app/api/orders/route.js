0f0ac036ea05d54a79208d8ec4bc8422eaf65a34
Deepak-suman <deepaksuman873@gmail.com>
2026-04-03 22:43:09 +0530

    rating update Issue

--- src/app/api/orders/route.js
+++ src/app/api/orders/route.js
@@ -1,6 +1,8 @@
 import { NextResponse } from "next/server";
 import { prisma } from "@/lib/prisma";
 
+export const dynamic = "force-dynamic";
+
 export async function POST(req) {
   try {
     const { tableNumber, customerName, cartItems } = await req.json();
